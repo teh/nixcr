812f186f4eda7a234d1d09ca0781d1187b64d228
Tom Hunger <tehunger@gmail.com>
2019-09-29 14:51:59 +0100

    pin nixpkgs + add missing dependencies

--- shell.nix
+++ shell.nix
@@ -1,4 +1,7 @@
-with import <nixpkgs> {};
+with import (fetchTarball {
+  url = https://github.com/NixOS/nixpkgs/archive/9691c53afc8f09a5d5a8acb50a2c2d56c0da6e10.tar.gz;
+  sha256 = "0j295k1gxnmx1wc88b4gn869qih6nnrkrqzj3kmp04cz2dk6ds50";
+}) {};
 
 stdenv.mkDerivation {
   name = "rust-env";
@@ -8,6 +11,11 @@ stdenv.mkDerivation {
   ];
   buildInputs = [
     openssl
+  ];
+  propagatedBuildInputs = [
+    nix
+    git
+    openssh
   ];
   RUST_BACKTRACE = 1;
 }
